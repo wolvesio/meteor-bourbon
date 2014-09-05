6d42750a850b8a05b4f65b2206758fbb0e74b71c
Christopher Stingl <cjstingl@gmail.com>
2014-09-05 14:57:55 -0400

    Removing unused pkg files + updating pkg.js
    Set for Meteor 0.9.0 add fourseven:scss dep

--- package.js
+++ package.js
@@ -1,16 +1,11 @@
 Package.describe({
-  summary: "A simple and lightweight mixin library for Sass. Built by Thoughtbot",
+  name: "wolves:bourbon",
+  summary: "Bourbon is a simple and lightweight mixin library for Sass.",
   version: "0.1.0",
   git: "https://github.com/wolvesio/meteor-bourbon"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.1');
-  api.addFiles('wolves:bourbon.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('wolves:bourbon');
-  api.addFiles('wolves:bourbon-tests.js');
+  api.versionsFrom('METEOR@0.9.0');
+  api.use('fourseven:scss@0.9.5', ['client', 'server']);
 });
