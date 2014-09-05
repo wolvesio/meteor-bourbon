85cf5e95630bb5e8fec6c60e67aa81b9f2a0cdc0
Christopher Stingl <cjstingl@gmail.com>
2014-09-05 16:21:17 -0400

    Reworking the pkg organization

--- package.js
+++ package.js
@@ -1,11 +1,15 @@
 Package.describe({
   name: "wolves:bourbon",
   summary: "Bourbon is a simple and lightweight mixin library for Sass.",
-  version: "0.1.0",
+  version: "0.2.0",
   git: "https://github.com/wolvesio/meteor-bourbon"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use('fourseven:scss@0.9.5', ['client', 'server']);
+  api.use('fourseven:scss@0.9.5', 'client');
+
+  var path = Npm.require('path');
+  var asset_path = path.join('bourbon');
+  api.addFiles(path.join(asset_path, '_bourbon.scss'), 'client');
 });
